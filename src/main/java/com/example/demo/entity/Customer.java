691e16f2b993240f4a6e52cf0ee3b38c8090df1a
Ankitha04 <tcslearning06@gmail.com>
2022-01-27 19:26:58 +0530

    deposit

--- src/main/java/com/example/demo/entity/Customer.java
+++ src/main/java/com/example/demo/entity/Customer.java
@@ -1,9 +1,12 @@
 package com.example.demo.entity;
 
+import javax.persistence.CascadeType;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.OneToOne;
+import javax.persistence.Table;
 import javax.validation.constraints.Email;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.Pattern;
@@ -12,6 +15,7 @@ import javax.validation.constraints.Size;
 import com.sun.istack.NotNull;
 
 @Entity
+@Table(name = "customer")
 public class Customer {
 
 	@Id
@@ -27,6 +31,7 @@ public class Customer {
 	// Gender
 	@NotBlank
 	@NotNull
+	@Pattern(regexp = "^M(ale)?$|^F(emale)?$")
 	private String gender;
 
 	// Date of birth
@@ -79,6 +84,10 @@ public class Customer {
 	@Size(max = 30)
 	@Pattern(regexp = "(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|\"(?:[\\x01-\\x08\\x0b\\x0c\\x0e-\\x1f\\x21\\x23-\\x5b\\x5d-\\x7f]|\\\\[\\x01-\\x09\\x0b\\x0c\\x0e-\\x7f])*\")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\\[(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?|[a-z0-9-]*[a-z0-9]:(?:[\\x01-\\x08\\x0b\\x0c\\x0e-\\x1f\\x21-\\x5a\\x53-\\x7f]|\\\\[\\x01-\\x09\\x0b\\x0c\\x0e-\\x7f])+)\\])")
 	private String email;
+
+	// Deposit
+	@OneToOne(cascade = CascadeType.ALL)
+	private Deposit deposit;
 
 	public Customer() {
 
@@ -162,6 +171,14 @@ public class Customer {
 
 	public void setCity(String city) {
 		this.city = city;
+	}
+
+	public Deposit getDeposit() {
+		return deposit;
+	}
+
+	public void setDeposit(Deposit deposit) {
+		this.deposit = deposit;
 	}
 
 }
--- src/main/java/com/example/demo/service/CustomerService.java
+++ src/main/java/com/example/demo/service/CustomerService.java
@@ -4,15 +4,22 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.example.demo.entity.Customer;
+import com.example.demo.entity.Deposit;
 import com.example.demo.repository.CustomerRepository;
+import com.example.demo.repository.DepositRepository;
 
 @Service
 public class CustomerService {
 
 	@Autowired
 	private CustomerRepository customerRepository;
+	
+	@Autowired
+	private DepositRepository depositRepository;
 
 	public void addUser(Customer customer) {
+		Deposit dep = customer.getDeposit();
+		depositRepository.save(dep);
 		customerRepository.save(customer);
 	}
 
