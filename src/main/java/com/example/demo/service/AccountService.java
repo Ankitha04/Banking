56755eaf76106e67f6065c00373224c7a1a27064
Ankitha04 <tcslearning06@gmail.com>
2022-01-27 20:41:49 +0530

    account

---
+++ src/main/java/com/example/demo/service/AccountService.java
@@ -0,0 +1,29 @@
+package com.example.demo.service;
+
+import javax.validation.Valid;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.example.demo.entity.Account;
+import com.example.demo.entity.Customer;
+import com.example.demo.repository.AccountRepository;
+import com.example.demo.repository.CustomerRepository;
+
+public class AccountService {
+
+	@Autowired
+	private AccountRepository accountRepository;
+	
+	@Autowired
+	private CustomerRepository customerRepository;
+
+
+	public void addAccount(@Valid Account account, Integer id) {
+		Customer customer = customerRepository.findById(id).get();
+		Account acc = customer.getAccount();
+
+		accountRepository.save(acc);
+		customerRepository.save(customer);
+
+	}
+}
--- src/main/java/com/example/demo/service/CustomerService.java
+++ src/main/java/com/example/demo/service/CustomerService.java
@@ -2,9 +2,10 @@ package com.example.demo.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-
+import com.example.demo.entity.Account;
 import com.example.demo.entity.Customer;
 import com.example.demo.entity.Deposit;
+import com.example.demo.repository.AccountRepository;
 import com.example.demo.repository.CustomerRepository;
 import com.example.demo.repository.DepositRepository;
 
@@ -16,11 +17,16 @@ public class CustomerService {
 	
 	@Autowired
 	private DepositRepository depositRepository;
+	
+	@Autowired
+	private AccountRepository accountRepository;
 
 	public void addUser(Customer customer) {
+		Account acc = customer.getAccount();
 		Deposit dep = customer.getDeposit();
 		depositRepository.save(dep);
 		customerRepository.save(customer);
+		accountRepository.save(acc);
 	}
 
 }
